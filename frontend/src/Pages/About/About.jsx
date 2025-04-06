ce981a6798ec87c43e672493c8c3070e8cf01426
nishumital3103 <nishumittal3103@gmail.com>
2025-04-06 23:54:46 +0530

    Add files via upload

---
+++ frontend/src/Pages/About/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <div id='about' className="about-page">
+      <h1>About Us</h1>
+      <p>Welcome to MY TREND! We are dedicated to providing the best products to our customers.</p>
+      <p>Our journey started in 2000, and we have been growing ever since, thanks to our loyal customers.</p>
+      {/* Add more content as needed */}
+    </div>
+  );
+};
+
+export default About;
