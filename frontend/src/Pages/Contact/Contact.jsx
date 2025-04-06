0d74969f6e7b53cf8f2dbd5fe6cdc0f4d5b787e5
nishumital3103 <nishumittal3103@gmail.com>
2025-04-06 23:55:38 +0530

    Add files via upload

---
+++ frontend/src/Pages/Contact/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Contact.css'
+
+const Contact = () => {
+  return (
+    <div id='contact' className="contact-page">
+      <h1>Contact Us</h1>
+      <p>If you have any questions, feel free to reach out to us.</p>
+      <p>Email: support@mytrend.com</p>
+      <p>Phone: +91 9034644134</p>
+      {/* Add more contact details as needed */}
+    </div>
+  );
+};
+
+export default Contact;
